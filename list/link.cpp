370faefb1015d03d79f6f22572344a469d762b25
G19970424 <1015636737@qq.com>
2024-03-15 15:52:59 +0800

    文件夹目录更新

---
+++ list/link.cpp
@@ -0,0 +1,14 @@
+#include "contains.cpp"
+#include "link.cpp"
+
+typedef struct LNode
+{
+    /* data */
+    LinkElemType date;
+    struct LNode *next;
+}LNode ,*LinkList;
+
+Status GetElem_L(LinkList L,int i,LinkElemType e){
+    
+}
+
